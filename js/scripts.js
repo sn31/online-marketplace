7c27113f44c85e1223a7ff539d65532a54cacefb
Meria Thomas and Skye Nguyen <meria@>
2018-08-08 14:05:32 -0700

    add products

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
+  var cart =[];
+  $("#book1").click(function() {
+    cart += "<br/>" + $("#book1").val();
+  });
+  $("#book2").click(function() {
+    cart += "<br/>" + $("#book2").val();
+  });
+  $("#book3").click(function() {
+    cart += "<br/>" + $("#book3").val();
+  });
 
   $("form#input").submit(function(event){
     event.preventDefault();
@@ -6,7 +16,9 @@ $(document).ready(function() {
     var address = $("#address").val();
     $(".name").text(name);
     $(".address").text(address);
+    $("#purchase").append(cart);
     $("#receipt").show();
   });
+
 
 });
